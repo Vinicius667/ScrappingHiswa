a3c194500c2144231c5d06972e42846312da664e
Vinícius Almeida <almeidabarrosdf@gmail.com>
2024-01-14 21:26:04 -0300

    Initial commit

---
+++ variables.py
@@ -0,0 +1,19 @@
+default_headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,"
+    "image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "DNT": "1",
+    "Connection": "close",
+    "Upgrade-Insecure-Requests": "1",
+    "Pragma": "no-cache",
+    "Cache-Control": "no-cache",
+    "SEC-CH-UA": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"",
+    "SEC-CH-UA-ARCH": "\"x86\"",
+    "SEC-CH-UA-BROWSER": "\"Chromium\"",
+    "SEC-CH-UA-MODE": "\"Regular\"",
+    "SEC-CH-UA-PLATFORM": "\"Windows\"",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+}
